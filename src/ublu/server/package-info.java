b7260826b9a95798d59d3ac0733cc611d95e86cf
jax <jax@localhost>
2016-08-04 15:04:45 -0600

    javadoc cleanup

--- src/ublu/server/package-info.java
+++ src/ublu/server/package-info.java
@@ -1,6 +1,6 @@
 /**
  * A server for the {@link ublu.Ublu} interpreter.
- * Most of the {@link Interpreter}'s functionality is accessible via
+ * Most of the {@link ublu.util.Interpreter}'s functionality is accessible via
  * TCP/IP once the "server -start" command is issued.
  */
 package ublu.server;
