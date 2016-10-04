cc27435c48978450046982a4077e72969b5817ad
jax <jax@localhost>
2016-10-04 09:54:22 -0600

    ublu -s

--- src/ublu/Ublu.java
+++ src/ublu/Ublu.java
@@ -28,6 +28,7 @@
 package ublu;
 
 import java.util.logging.Logger;
+import ublu.util.Generics.StringArrayList;
 
 import ublu.util.Interpreter;
 import ublu.util.InterpreterLogger;
@@ -119,6 +120,9 @@ public class Ublu {
                 .append(" http://pigiron.sourceforge.net\n")
                 .append("Copyright (c) 2008-2016 Jack J. Woehr, PO Box 51, Golden CO 80402 USA\n")
                 .append("All Rights Reserved")
+                .append("\n---\n")
+                .append("org.json\n")
+                .append("Copyright (c) 2002 JSON.org")
                 .toString();
     }
 
@@ -135,8 +139,9 @@ public class Ublu {
                 .append("Copyright 2016, Jack J. Woehr, http://www.softwoehr.com\n")
                 .append("All Rights Reserved\n")
                 .append("Ublu is Open Source Software under the BSD 2-clause license.\n")
+                .append("THERE IS NO WARRANTY and NO GUARANTEE OF CORRECTNESS NOR APPLICABILITY.\n")
                 .append("***\n")
-                .append("Ublu utilizes the following open source projects ")
+                .append("Ublu utilizes the following open source projects:")
                 .append(openSourceList())
                 .append("\n***")
                 .toString();
@@ -151,6 +156,7 @@ public class Ublu {
      */
     public Ublu(String[] args) {
         this();
+        args = processArgs(args);
         setMainInterpreter(new Interpreter(args, this));
         LOG = new InterpreterLogger("UbluInterpreter." + Thread.currentThread().toString(), Logger.getLogger(Ublu.class.getName()), getMainInterpreter().getErroutStream());
     }
@@ -169,9 +175,11 @@ public class Ublu {
     public int runMainInterpreter() {
         Interpreter interpreter = getMainInterpreter();
         if (interpreter.getArgArray().isEmpty()) {
-            if (interpreter.isConsole()) {
-                interpreter.outputerrln(startupMessage());
-                interpreter.outputerrln(EXITLINE);
+            if (!getSwitches().contains("-s")) {
+                if (interpreter.isConsole()) {
+                    interpreter.outputerrln(startupMessage());
+                    interpreter.outputerrln(EXITLINE);
+                }
             }
             interpreter.interpret();
         } else {
@@ -194,7 +202,10 @@ public class Ublu {
      * last executed or
      * {@link ublu.command.CommandInterface.COMMANDRESULT#FAILURE} (1).</p>
      *
-     * @param args commands to program in the program's syntax
+     * @param args commands to program in the program's syntax. Any strings
+     * starting with a dash <code>-</code> are taken to be switches to the ublu
+     * invocation itself and are processed and removed from the args passed to
+     * the interpreter.
      */
     public static void main(String[] args) {
         System.exit(niam(args));
@@ -206,7 +217,10 @@ public class Ublu {
      * the retval of the last command. Useful for calling Ublu from another Java
      * program.
      *
-     * @param args commands to program in the program's syntax
+     * @param args commands to program in the program's syntax. Any strings
+     * starting with a dash <code>-</code> are taken to be switches to the ublu
+     * invocation itself and are processed and removed from the args passed to
+     * the interpreter.
      * @return retval of the last command executed, either
      * {@link ublu.command.CommandInterface.COMMANDRESULT#SUCCESS} (0) if the
      * last executed normally or otherwise
@@ -215,5 +229,61 @@ public class Ublu {
     public static int niam(String[] args) {
         Ublu api = new Ublu(args);
         return api.runMainInterpreter();
+    }
+
+    private StringArrayList originalArgs;
+
+    /**
+     * Get the value of originalArgs
+     *
+     * @return the value of originalArgs
+     */
+    public StringArrayList getOriginalArgs() {
+        return originalArgs;
+    }
+
+    private StringArrayList switches = new StringArrayList();
+
+    /**
+     * Get the value of switches
+     *
+     * @return the value of switches
+     */
+    public StringArrayList getSwitches() {
+        return switches;
+    }
+
+    /**
+     * Set the value of switches
+     *
+     * @param switches new value of switches
+     */
+    public void setSwitches(StringArrayList switches) {
+        this.switches = switches;
+    }
+
+    /**
+     * Set the value of originalArgs
+     *
+     * @param originalArgs new value of originalArgs
+     */
+    public void setOriginalArgs(StringArrayList originalArgs) {
+        this.originalArgs = originalArgs;
+    }
+
+    private String[] processArgs(String[] args) {
+        StringArrayList remainderArgs = new StringArrayList(args);
+        setOriginalArgs(new StringArrayList(args));
+        for (String s : getOriginalArgs()) {
+            if (s.startsWith("-")) {
+                getSwitches().add(s);
+            } else {
+                break;
+            }
+        }
+        for (String switche : getSwitches()) {
+            remainderArgs.remove(0);
+        }
+        return remainderArgs.toStringArray();
     }
 }
