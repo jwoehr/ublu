c20d71f17bdc689ba5198889d8d1f665baea04c8
jax <jax@localhost>
2016-10-05 10:28:12 -0600

    correct description

--- src/ublu/command/CmdList.java
+++ src/ublu/command/CmdList.java
@@ -49,7 +49,7 @@ public class CmdList extends Command {
 
     {
         setNameAndDescription("list",
-                "/0 [-to datasink] [--,-list @list] [[-instance] | [-source ~@enumeration|~@collection|~@string|-@array] | [-add ~@object ] | [-addstr ~@{ some string }] | [-clear] | [-get ~@{intindex}] | [-set ~@{intindex} ~@object] | [-remove ~@object] | [-removeat ~@{intindex}] | [-size] | [-toarray]]: create and manage lists of objects");
+                "/0 [-to datasink] [--,-list ~@list] [[-instance] | [-source ~@enumeration|~@collection|~@string|-@array] | [-add ~@object ] | [-addstr ~@{ some string }] | [-clear] | [-get ~@{intindex}] | [-set ~@{intindex} ~@object] | [-remove ~@object] | [-removeat ~@{intindex}] | [-size] | [-toarray]]: create and manage lists of objects");
     }
 
     /**
