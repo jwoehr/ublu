1dacf7f260a16eaf4ce13bea925b8864c3446be4
jax <jax@localhost>
2016-10-11 12:18:56 -0600

    setDataDest/SrcFromArgArray

--- src/ublu/command/CmdCollection.java
+++ src/ublu/command/CmdCollection.java
@@ -67,8 +67,7 @@ public class CmdCollection extends Command {
             String dashCommand = argArray.parseDashCommand();
             switch (dashCommand) {
                 case "-to":
-                    String destName = argArray.next();
-                    setDataDest(DataSink.fromSinkName(destName));
+                    setDataDestfromArgArray(argArray);
                     break;
 //                case "-from":
 //                    String srcName = argArray.next();
