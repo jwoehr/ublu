338ee1b4b30020151a0165613e1b2e60d2454db9
Taylor C. Richberger <Taywee@gmx.com>
2016-12-11 16:45:25 -0700

    get lexer working

--- src/ublu/lexer/Lexer.java
+++ src/ublu/lexer/Lexer.java
@@ -1,19 +1,17 @@
+package ublu.lexer;
+
 import java.io.IOException;
 import java.util.List;
 import org.antlr.v4.runtime.*;
 import org.antlr.v4.runtime.tree.ParseTree;
 
 public class Lexer {
-    public List<ParseTree> parseFile(String filename) {
-        try {
-            ANTLRFileStream file = new ANTLRFileStream(filename);
-            UbluLexer lexer = new UbluLexer(file);
-            CommonTokenStream tokens = new CommonTokenStream(lexer);
-            UbluParser parser = new UbluParser(tokens);
-            UbluParser.ProgContext tree = parser.prog();
-            return tree.children;
-        } catch (IOException e) {
-            e.printStackTrace(System.err);
-        }
+    public List<ParseTree> parseFile(String filename) throws IOException {
+        ANTLRFileStream file = new ANTLRFileStream(filename);
+        UbluLexer lexer = new UbluLexer(file);
+        CommonTokenStream tokens = new CommonTokenStream(lexer);
+        UbluParser parser = new UbluParser(tokens);
+        UbluParser.ProgContext tree = parser.prog();
+        return tree.children;
     }
 }
