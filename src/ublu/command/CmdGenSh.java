89afd0a9c382de0994b2b2aa55e0bb539d0df4b2
Taylor C. Richberger <Taywee@gmx.com>
2016-12-21 13:46:23 -0700

    documentation and some performance changes

--- src/ublu/command/CmdGenSh.java
+++ src/ublu/command/CmdGenSh.java
@@ -47,7 +47,7 @@ public class CmdGenSh extends Command {
 
     {
         setNameAndDescription("gensh",
-                "/5+ [-to datasink] [-strictPosix] [[-path fullyqualifiedjarpath] [-includepath searchpath] [-opt optchar assignment_name tuplename ${ description }$ ..] [-optr optchar assignment_name tuplename ${ description }$] [-optx optchar multiple_assignment_name tuplename ${ description }$ ..]] ~@${scriptname} ~@${includename}$ ~@${ functionCall ( @a @b ... ) }$ : generate launcher shell script");
+                "/5+ [-to datasink] [-strictPosix] [[-path fullyqualifiedjarpath] [-includepath ~@${searchpath}] [-opt optchar assignment_name tuplename ${ description }$ ..] [-optr optchar assignment_name tuplename ${ description }$] [-optx optchar multiple_assignment_name tuplename ${ description }$ ..]] ~@${scriptname} ~@${includename}$ ~@${ functionCall ( @a @b ... ) }$ : generate launcher shell script");
     }
 
     /**
@@ -91,7 +91,7 @@ public class CmdGenSh extends Command {
                     genSh.accumulateCommand(genSh.getFqJarPath());
                     break;
                 case "-includepath":
-                    genSh.setIncludePath(argArray.next());
+                    genSh.setIncludePath(argArray.nextMaybeQuotationTuplePopString());
                     genSh.accumulateCommand(genSh.getIncludePath());
                     break;
                 default:
