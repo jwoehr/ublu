adef4dc93212a3119fc1ea646c1672a511d2c7a0
jax <jax@varian>
2017-08-13 21:03:10 -0600

    cleanup

--- src/ublu/command/CmdMonitor.java
+++ src/ublu/command/CmdMonitor.java
@@ -38,7 +38,6 @@ import java.beans.PropertyVetoException;
 import java.io.IOException;
 import java.sql.SQLException;
 import java.util.logging.Level;
-import ublu.util.Tuple;
 
 /**
  * Command to run System Shepherd-style monitoring.
