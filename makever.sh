cab76ba2fe818ee6809e706681e674904042ce60
Jesse Gorzinski <jgorzinski@gmail.com>
2022-01-31 21:58:12 -0600

    fixup

--- makever.sh
+++ makever.sh
@@ -6,6 +6,6 @@ import java.text.DateFormat;
 import java.util.Date;
 
 class Version {
-    static String compileDateTime =DateFormat.getDateInstance(DateFormat.LONG).format(new Date());
+    static String compileDateTime =DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG).format(new Date());
     static String ubluVersion = \"$1\";
 }" > src/main/java/ublu/Version.java
