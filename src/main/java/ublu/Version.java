0c23ee92f22270a9d1b7545ddabc69b9f02aa9eb
jwoehr <jwoehr@softwoehr.com>
2022-08-06 05:48:41 -0600

    postgresql bump

--- src/main/java/ublu/Version.java
+++ src/main/java/ublu/Version.java
@@ -1,5 +1,5 @@
 package ublu;
 class Version {
-      static String compileDateTime = "2022-06-05 03:23:26 (GMT)";
+      static String compileDateTime = "2022-08-06 11:46:19 (GMT)";
       static String ubluVersion = "2.0.1-dev";
 }
