05d4fad9228cf265d623ae59ae426ac61716ee1e
jwoehr <jwoehr@softwoehr.com>
2024-08-05 15:21:26 -0600

    start version 2.0.3-dev

--- src/main/java/ublu/Version.java
+++ src/main/java/ublu/Version.java
@@ -1,5 +1,5 @@
 package ublu;
 class Version {
-      static String compileDateTime = "2024-08-05 20:53:54 (GMT)";
-      static String ubluVersion = "2.0.2";
+      static String compileDateTime = "2024-08-05 21:17:48 (GMT)";
+      static String ubluVersion = "2.0.3-dev";
 }
