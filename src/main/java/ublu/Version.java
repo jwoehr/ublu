3da01b650e9dba7c47b5740b7c211344cf41130b
jwoehr <jwoehr@softwoehr.com>
2024-08-05 14:57:20 -0600

    update supporting libraries

--- src/main/java/ublu/Version.java
+++ src/main/java/ublu/Version.java
@@ -1,5 +1,5 @@
 package ublu;
 class Version {
-      static String compileDateTime = "2024-04-04 04:48:06 (GMT)";
-      static String ubluVersion = "2.0.2-dev";
+      static String compileDateTime = "2024-08-05 20:53:54 (GMT)";
+      static String ubluVersion = "2.0.2";
 }
