2c8f66c28e4973e59693870b3b73ca1d77f07db7
jwoehr <jwoehr@softwoehr.com>
2024-10-18 09:49:05 -0600

    updated postgresql driver to 42.7.4

--- src/main/java/ublu/Version.java
+++ src/main/java/ublu/Version.java
@@ -1,5 +1,5 @@
 package ublu;
 class Version {
-      static String compileDateTime = "2024-08-05 21:17:48 (GMT)";
+      static String compileDateTime = "2024-10-18 15:43:38 (GMT)";
       static String ubluVersion = "2.0.3-dev";
 }
