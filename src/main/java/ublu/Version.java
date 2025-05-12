777c2cfc1f03655333787496f35ca0f42f2e07e9
jwoehr <jwoehr@softwoehr.com>
2025-05-12 14:21:04 -0600

    - update some artifacts - delete local artifacts no longer used

--- src/main/java/ublu/Version.java
+++ src/main/java/ublu/Version.java
@@ -1,5 +1,5 @@
 package ublu;
 class Version {
-      static String compileDateTime = "2025-04-04 15:36:42 (GMT)";
+      static String compileDateTime = "2025-05-12 20:19:17 (GMT)";
       static String ubluVersion = "2.0.5-dev";
 }
