00d1eaaf79777354feeee461f78e099c90df06a3
jwoehr <jwoehr@softwoehr.com>
2025-08-09 09:31:55 -0600

    - v2.0.5 - added public key

--- src/main/java/ublu/Version.java
+++ src/main/java/ublu/Version.java
@@ -1,5 +1,5 @@
 package ublu;
 class Version {
-      static String compileDateTime = "2024-12-17 18:48:52 (GMT)";
-      static String ubluVersion = "2.0.3-dev";
+      static String compileDateTime = "2025-08-09 15:28:52 (GMT)";
+      static String ubluVersion = "2.0.5";
 }
