897c2ce0f0e0d29c91d66ba4bea110486d3ab5d7
jwoehr <jwoehr@softwoehr.com>
2026-04-13 21:02:35 -0600

    update version

--- src/main/java/ublu/Version.java
+++ src/main/java/ublu/Version.java
@@ -1,5 +1,5 @@
 package ublu;
 class Version {
-      static String compileDateTime = "2025-11-07 16:17:51 (GMT)";
-      static String ubluVersion = "2.0.6";
+      static String compileDateTime = "2026-04-14 03:00:37 (GMT)";
+      static String ubluVersion = "2.0.6-dev";
 }
