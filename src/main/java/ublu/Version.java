d8c7696d5622f2d7e5a7f44d57f84b8c434a440e
jwoehr <jwoehr@softwoehr.com>
2026-06-21 11:37:12 -0600

    that usual skew of Version.java

--- src/main/java/ublu/Version.java
+++ src/main/java/ublu/Version.java
@@ -1,5 +1,5 @@
 package ublu;
 class Version {
-      static String compileDateTime = "2026-05-18 21:21:38 (GMT)";
+      static String compileDateTime = "2026-06-21 16:09:59 (GMT)";
       static String ubluVersion = "2.0.6-dev";
 }
